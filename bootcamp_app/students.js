7d756cd85e23dd3c93542befaef4f681e184dc91
Dasha Frumovitch <dfrumovitch@gmail.com>
2019-11-14 17:14:35 +0000

    query sql through node - specific cohort - set limit

---
+++ bootcamp_app/students.js
@@ -0,0 +1,28 @@
+// const { Pool } = require('pg');
+// const pool = new Pool();
+
+const { Pool } = require('pg');
+// const process = ('process');
+
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+pool.query(`
+      SELECT students.id, students.name, cohorts.name AS cohort
+      FROM students
+      JOIN cohorts ON cohorts.id = cohort_id
+      WHERE cohorts.name LIKE '%${process.argv[2]}%'
+      LIMIT ${process.argv[3] || 5};
+`)
+.then(res => {
+  // console.log(res);
+  res.rows.forEach(user => {
+    console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort} cohort`);
+  })
+})
+.catch(err => console.error('query error', err.stack));
