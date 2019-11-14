e55debe6d5ee05e1e972c3bfae92d6b6ac99dcb3
Dasha Frumovitch <dfrumovitch@gmail.com>
2019-11-14 17:15:34 +0000

    query sql through node - all teachers assisted - set specific cohort

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,26 @@
+const { Pool } = require('pg');
+
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+pool.query(`
+      SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
+      FROM teachers
+      JOIN assistance_requests ON teacher_id = teachers.id
+      JOIN students ON student_id = students.id
+      JOIN cohorts ON cohort_id = cohorts.id
+      WHERE cohorts.name LIKE '%${process.argv[2]}%'
+      ORDER BY teacher;
+`)
+.then(res => {
+  // console.log(res);
+  res.rows.forEach(row => {
+    console.log(`${row.cohort}: ${row.teacher}`);
+  })
+})
+.catch(err => console.error('query error', err.stack));
